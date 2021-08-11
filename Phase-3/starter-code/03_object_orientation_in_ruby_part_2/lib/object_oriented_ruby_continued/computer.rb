d984cfb199064baef49db282744d4f8f9f38fa9a
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-08-11 11:23:47 -0700

    reset starter code

--- Phase-3/starter-code/03_object_orientation_in_ruby_part_2/lib/object_oriented_ruby_continued/computer.rb
+++ Phase-3/starter-code/03_object_orientation_in_ruby_part_2/lib/object_oriented_ruby_continued/computer.rb
@@ -1,12 +1,4 @@
 class Computer
-
-  def initialize(brand, screen_size, model_name, model_year)
-    @brand = brand
-    @screen_size = screen_size
-    @model_name = model_name
-    @model_year = model_year
-  end
-
   def brand
     @brand
   end
@@ -56,20 +48,30 @@ class Computer
   end
 
   def last_backed_up_at
-    (@last_backed_up_at && @last_backed_up_at.strftime("%m-%e-%y %H:%M")) || "never"
+    (@last_backed_up_at && @last_backed_up_at.strftime("on %m/%d/%y at %H:%M")) || "never"
   end
 
   def about_this_computer
-    "This computer is a #{@model_year} #{@brand} #{@model_name} with a #{@screen_size} inch screen. It was last backed up at#{@last_backed_up_at.strftime("%m-%e-%y %H:%M")}"
+    "This computer is a #{@model_year} #{@brand} #{@model_name} with a #{@screen_size} inch screen. It was last backed up #{last_backed_up_at}"
   end
 end
 
-# this time, assign all of the computer's attributes using .new instead of calling the setters directly
+# create and return the first computer within this method
 def first_computer
-  Computer.new("Apple", 15.0, "Macbook Pro", 2012)
+  computer = Computer.new
+  computer.brand = "Apple"
+  computer.screen_size = 15.0
+  computer.model_name = "Macbook Pro"
+  computer.model_year = 2012
+  computer
 end
 
-# this time, assign all of the computer's attributes using .new instead of calling the setters directly
+# create and return the second computer within this method
 def second_computer
-  Computer.new("Apple", 24.0, "iMac", 2021)
+  computer = Computer.new
+  computer.brand = "Apple"
+  computer.screen_size = 13.0
+  computer.model_name = "Macbook Air"
+  computer.model_year = 2015
+  computer
 end
